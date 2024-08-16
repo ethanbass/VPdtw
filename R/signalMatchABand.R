f461707eea90499b7d17d8cc4d3554fe33625329
Ethan Bass <ethanbass@gmail.com>
2024-08-16 16:54:23 -0400

    style: whitespace (signalMatchABand)

--- R/signalMatchABand.R
+++ R/signalMatchABand.R
@@ -14,15 +14,16 @@
 ###
 ### s is assumed to be reference and r is aligned to it in this version.
 
+#' @noRd
 signalMatchABand <- function(reference, query,
                              lambda = rep(0.0, length(reference)),
                              maxshift = 50){
 
   nr <- length(reference)
   nq <- length(query)
-  ## lambda <- rep(0,length(reference));maxshift=20
-  if(nq + maxshift < nr) stop("length(query) + maxshift should be greater than or equal to length(reference)\n")
-  ## if(length(query) != n) stop("Signals need to same length in this implementation")
+
+  if(nq + maxshift < nr)
+    stop("length(query) + maxshift should be greater than or equal to length(reference) \n")
 
   pp <- .C("signalMatchWrapABand",
            reference = as.double(reference),
@@ -36,41 +37,43 @@ signalMatchABand <- function(reference, query,
            PACKAGE = "VPdtw")
 
   path <- pp$path
-  path[path==0] <- NA
+  path[path == 0] <- NA
   minp <- min(path, na.rm = TRUE)
   maxp <- max(path, na.rm = TRUE)
 
-  xIndices <- path; xVals <- 1:length(path)##pp$reference)
+  xIndices <- path
+  xVals <- 1:length(path)
   if (minp > 1) {
-    xIndices <- c(1:(minp-1), xIndices)
-    xVals <- c(seq(to=0, len=minp-1, by=1),xVals)
+    xIndices <- c(1:(minp - 1), xIndices)
+    xVals <- c(seq(to = 0, len = minp - 1, by = 1), xVals)
   }
   if(maxp < length(query)) {
-    xIndices <- c(xIndices,(maxp+1):length(query))
-    xVals <- c(xVals,seq(from=max(xVals)+1,by=1,len=length(query)-(maxp)))
+    xIndices <- c(xIndices,(maxp + 1):length(query))
+    xVals <- c(xVals, seq(from = max(xVals) + 1, by = 1,
+                         len = length(query) - (maxp)))
   }
 
   if (FALSE) {
-    plot(reference,type="l",lwd=2,xlim=c(1-maxshift,nr+maxshift))
-    lines(which(!is.na(path)),query[na.omit(path)],col=2)
-    lines(xVals,query[xIndices],col=3,lty=2)
+    plot(reference, type="l", lwd=2, xlim = c(1-maxshift, nr + maxshift))
+    lines(which(!is.na(path)), query[na.omit(path)], col = 2)
+    lines(xVals, query[xIndices], col = 3, lty = 2)
   }
 
   ## Should we warn when we get close to maxshift?
   shift <- xVals - xIndices
-  if (max(abs(shift), na.rm=TRUE) > (3*maxshift/4))
+  if (max(abs(shift), na.rm = TRUE) > (3*maxshift/4))
     warning("Observed shift more than three quarters of maxshift")
 
   ## Come up with a nice summary
-  output <- matrix(NA,length(xVals),4)
-  colnames(output) <- c("xVals","reference","warped query","shift")
-  output[,"xVals"] <- xVals
+  output <- matrix(NA, length(xVals), 4)
+  colnames(output) <- c("xVals", "reference", "warped query", "shift")
+  output[, "xVals"] <- xVals
   str <- which(xVals == 1)
   end <- which(xVals == nr)
-  output[seq(str, end, by=1),"reference"] <- reference
+  output[seq(str, end, by = 1),"reference"] <- reference
 
-  output[,"warped query"] <- query[xIndices]
-  output[,"shift"] <- shift
+  output[, "warped query"] <- query[xIndices]
+  output[, "shift"] <- shift
 
   zz <- output
   return(invisible(zz))
